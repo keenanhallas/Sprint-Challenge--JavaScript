c1fd1fbc8f691b3f8c7c6407398f8dc31a8d29e1
Keenan <keenanhallas@gmail.com>
2020-03-20 19:51:18 -0400

    class challenges complete, along with stretch goals

--- challenges/prototypes.js
+++ challenges/prototypes.js
@@ -28,7 +28,7 @@ CuboidMaker.prototype.volume = function() {
 */
 
 CuboidMaker.prototype.surfaceArea = function() {
-  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height))
+  return 2 * ((this.length * this.width) + (this.length * this.height) + (this.width * this.height));
 }
 
 /* == Step 4: Create a new object that uses CuboidMaker ==
@@ -36,10 +36,10 @@ CuboidMaker.prototype.surfaceArea = function() {
   Add properties and values of length: 4, width: 5, and height: 5 to cuboid.   
 */
 
-cuboid = new CuboidMaker(4, 5, 5);
+// cuboid = new CuboidMaker(4, 5, 5);
 
 // Test your volume and surfaceArea methods by uncommenting the logs below:
-console.log(cuboid.volume()); // 100
-console.log(cuboid.surfaceArea()); // 130
+// console.log(cuboid.volume()); // 100
+// console.log(cuboid.surfaceArea()); // 130
 
 
